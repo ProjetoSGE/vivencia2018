8b90c0f2d174499a386dae204fe51df3e316788c
infod32 <infod32@INFO04>
2018-11-27 17:19:28 -0200

    Pastas Criadas DTO  Cadidato

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Classes/Classes/Candidato/CandidatoDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1.Classes.Classes.Candidato
+{
+    public class CandidatoDTO
+    {
+        public int ID { get; set; }
+
+        public int IdAluno { get; set; }
+
+        public int IdVaga  { get; set; }
+
+        public string Status { get; set; }
+
+
+    }
+}
