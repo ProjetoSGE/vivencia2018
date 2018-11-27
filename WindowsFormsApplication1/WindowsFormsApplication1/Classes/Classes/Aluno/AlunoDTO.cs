429537cc0819969b83cc125fef77f77a805d7465
infod32 <infod32@INFO04>
2018-11-27 17:42:02 -0200

    ((DTO'S))

--- WindowsFormsApplication1/WindowsFormsApplication1/Classes/Classes/Aluno/AlunoDTO.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Classes/Classes/Aluno/AlunoDTO.cs
@@ -8,8 +8,23 @@ namespace WindowsFormsApplication1.Classes.Classes.Aluno
 {
     public class AlunoDTO
     {
-
-
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string NomeCurso { get; set; }
+        public string AnoDeEstudo { get; set; }
+        public DateTime Nascimento { get; set; }
+        public int Idade { get; set; }
+        public string Endereco { get; set; }
+        public int Numero { get; set; }
+        public string Bairro { get; set; }
+        public string CEP { get; set; }
+        public string Fixo { get; set; }
+        public string Celular { get; set; }
+        public string SeEstuda { get; set; }
+        public string QualCurso { get; set; }
+        public string Turno { get; set; }
+        public string ExpProfissional { get; set; }
+        public string AreaPreferencial { get; set; }
 
     }
 }
--- WindowsFormsApplication1/WindowsFormsApplication1/Classes/Classes/Empresa/EmpresaDTO.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Classes/Classes/Empresa/EmpresaDTO.cs
@@ -8,5 +8,13 @@ namespace WindowsFormsApplication1.Classes.Classes.Empresa
 {
     public class EmpresaDTO
     {
+        public int ID { get; set; }
+        public string Empresa { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string CEP { get; set; }
+        public string Bairro { get; set; }
+        public string Endereço { get; set; }
+        public string PontoDeReferencia { get; set; }
     }
 }
